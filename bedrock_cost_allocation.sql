423e3cb797e852c8ae1a437e3ce949d991b40bb5
Steph Gooch <awssteph@amazon.com>
2025-05-27 12:05:09 +0100

    updated queries

--- bedrock_cost_allocation.sql
+++ bedrock_cost_allocation.sql
@@ -9,40 +9,36 @@ SELECT line_item_usage_type,
 	SPLIT(billing_period, '-') [ 2 ] as month
 FROM "cid_data_export"."cur2"
 Where (
-		line_item_product_code like '%Bedrock%'
-		or product [ 'product_name' ] like '%Bedrock%'
-	) AND (line_item_usage_type like '%token%') --not with the S 
-
+		line_item_product_code like '%Bedrock%'or product [ 'product_name' ] like '%Bedrock%')  -- example p_n = 'Claude 3 Haiku (Amazon Bedrock Edition)'
+	 
+	 AND (line_item_usage_type like '%token%' or line_item_usage_type LIKE '%Token%') --not with the S because  Marketplace 'USW2-MP:USW2_InputTokenCount-Units' 
 --------------------------------
 
 with token_data as (
-	SELECT line_item_resource_id,
-		line_item_usage_type,
+	SELECT line_item_usage_type,
 		line_item_product_code,
 		product [ 'product_name' ] as product_name,
 		line_item_operation,
 		line_item_usage_amount,
 		line_item_unblended_cost,
-		CASE
-			WHEN (line_item_usage_type LIKE '%input-token%' OR  line_item_usage_type LIKE '%InputToken%') THEN 'Input' -- both 1,2,3P
-
-			WHEN (line_item_usage_type LIKE '%output-token%' OR  line_item_usage_type LIKE '%OutToken%') THEN 'Input' -- both 1,2,3P
-			ELSE 'Other'
-			
-		END AS token_type,
+		line_item_resource_id,
 		resource_tags [ 'user_dept' ] as user_dept,
 		SPLIT(billing_period, '-') [ 2 ] as month
 	FROM "cid_data_export"."cur2"
 	Where (
 			line_item_product_code like '%Bedrock%'
 			or product [ 'product_name' ] like '%Bedrock%'
+		) -- example p_n = 'Claude 3 Haiku (Amazon Bedrock Edition)'
+		AND (
+			line_item_usage_type like '%token%'
+			or line_item_usage_type LIKE '%Token%'
+		) --not with the S because  Marketplace 'USW2-MP:USW2_InputTokenCount-Units' 
 		)
-		AND (line_item_usage_type like '%token%') --not with the S 
-)
-SELECT DISTINCT CASE
-		when user_dept is not null then user_dept Else 'untagged'
-	end as tag,
-	month,
-	SUM(line_item_unblended_cost) OVER (PARTITION BY user_dept, month) AS total_cost
-FROM token_data
-where month = '05'
+	select distinct CASE
+			when user_dept is not null then user_dept else 'untagged'
+		END as tag,
+		month,
+		sum(line_item_unblended_cost) over (partition by user_dept, month) as total_cost, 
+			sum(line_item_usage_amount) over (partition by user_dept, month) as total_usage
+	from token_data
+	where month = '05'
